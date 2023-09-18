eacdb23fe54fe8ab3b6af6c68d4b4b4503b76ba9
JoaoSapata <142444143+JoaoSapata@users.noreply.github.com>
2023-09-18 23:32:40 +0000

    começo java

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function calculaCombustivel(event){
+    event.preventDefault()
+
+    let preco, gasto, distancia = 0
+    preco = document.getElementById("preco").value
+    gasto = document.getElementById("gasto").value
+    distancia = document.getElementById("distancia").value
+    consumo = distancia / gasto
+    gastoTotal = consumo * preco
+  
+
+document.getElementById("resultado").innerHTML
