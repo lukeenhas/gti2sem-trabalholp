832f2710a3ea2955e0630c66242a8ebec2e38c36
Lucas Rodrigues <lucasrodrigueshornett@gmail.com>
2023-09-25 20:55:05 -0300

    atualizacao js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 function calculaCombustivel(event){
     event.preventDefault()
 
-    let preco, gasto, distancia = 0
-    preco = document.getElementById("preco").value
-    gasto = document.getElementById("gasto").value
-    distancia = document.getElementById("distancia").value
-    consumo = distancia / gasto
-    gastoTotal = consumo * preco
-  
+    let nome, unome, combustivel  = "";
+    let preco, gasto, distancia = 0;
 
-document.getElementById("resultado").innerHTML = "teste";
+    preco = document.getElementById("preco").value;
+    gasto = document.getElementById("gasto").value;
+    distancia = document.getElementById("distancia").value;
+    nome = document.getElementById("pnome").value;
+    unome = document.getElementById("unome").value;
+    combustivel = document.getElementById("combustivel").value;
 
+    consumo = distancia / gasto;
+    gastoTotal = consumo * preco;
+
+document.getElementById("resultado").innerHTML = `${nome} ${unome} você usou ${combustivel} que estava no valor de R$${preco} totabilizando 
+um gasto total de: R$${gastoTotal.toFixed(2)}` ;
 }
